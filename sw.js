36e80c8e396d5361898aeb911d928beb37955716
ewertonlvale <ewertonlv@gmail.com>
2026-08-05 23:13:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "triduo-v11";
+const CACHE = "triduo-v12";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
